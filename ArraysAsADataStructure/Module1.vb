3ee4c1ac03d5d9a766c071c4cfc92d80c4577ee6
A Stone <astone@heckgrammar.co.uk>
2023-01-24 13:49:59 +0000

    Add project files.

---
+++ ArraysAsADataStructure/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim myArray(9) As Integer
+
+        Dim anotherArray = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+        Dim yetAnotherArray = New Integer() {9, 8, 7, 6, 5, 4, 3, 2, 1}
+
+        For i = 0 To myArray.Length - 1
+            myArray(i) = i
+        Next
+
+    End Sub
+
+End Module
---
+++ ArraysAsADataStructure/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ArraysAsADataStructure")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HGS-SCCM")>
+<Assembly: AssemblyProduct("ArraysAsADataStructure")>
+<Assembly: AssemblyCopyright("Copyright © HGS-SCCM 2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("53579496-3df4-4c99-8afc-84215912b6d0")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
